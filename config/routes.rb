4558f6de2f2cb6d13eb94751c168494030b94e2d
mfrkw <xjdk3766@gmail.com>
2021-08-10 08:28:28 +0000

    [topページ作成]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :fosters
-  devise_for :members
+  root to: 'homes#top'
+  
+  devise_for :fosters, controllers: {
+    sessions:      'fosters/sessions',
+    passwords:     'fosters/passwords',
+    registrations: 'fosters/registrations'
+  }
+  
+  devise_for :members, controllers: {
+    sessions:      'members/sessions',
+    passwords:     'members/passwords',
+    registrations: 'members/registrations'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
