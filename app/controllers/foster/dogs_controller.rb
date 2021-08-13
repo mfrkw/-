9d01bc4fe039ff683f0f152d2e2a9d9566151461
mfrkw <xjdk3766@gmail.com>
2021-08-13 12:41:28 +0000

    [フラッシュメッセージ]

--- app/controllers/foster/dogs_controller.rb
+++ app/controllers/foster/dogs_controller.rb
@@ -1,17 +1,17 @@
 class Foster::DogsController < ApplicationController
-  
+
   def index
     @dogs = Dog.all
-  end 
-  
+  end
+
   def show
     @dog = Dog.find(params[:id])
-  end 
-  
-  def new 
+  end
+
+  def new
     @dog = Dog.new
-  end 
-  
+  end
+
   def create
     @dog = Dog.new(dog_params)
     @dog.foster_id = current_foster.id
@@ -19,27 +19,27 @@ class Foster::DogsController < ApplicationController
     redirect_to  thanx_foster_dogs_path, notice: '登録が完了しました'
     else
       render :new, notice: '登録に失敗しました'
-    end 
+    end
   end
-  
-  def edit 
+
+  def edit
     @dog = Dog.find(params[:id])
   end
-  
+
   def update
     dog = Dog.find(params[:id])
     dog.update!(dog_params)
-    redirect_to foster_dog_path(dog.id), notice: '変更'
-  end 
-  
+    redirect_to foster_dog_path(dog.id), notice: '変更しました'
+  end
+
   def thanx
   end
-  
+
   private
-  
+
   def dog_params
     params.require(:dog).permit(:name, :area, :image, :introduction, :dog_breed, :foster_id)
   end
-    
-  
+
+
 end
--- app/controllers/foster/fosters_controller.rb
+++ app/controllers/foster/fosters_controller.rb
@@ -1,32 +1,31 @@
 class Foster::FostersController < ApplicationController
-  
+
   def show
     @foster = Foster.find(params[:id])
-  end 
-  
+  end
+
   def edit
     @foster = Foster.find(params[:id])
-  end 
-  
+  end
+
   def update
     foster = Foster.find(current_foster.id)
     if foster.update(foster_params)
-      flash[:success] = "登録情報を変更しました"
-      redirect_to foster_foster_path
+      redirect_to foster_foster_path, notice: '変更しました'
     else
       render :edit
-    end 
-  end 
-  
+    end
+  end
+
   def out
     @foster = Foster.find(current_foster.id)
     @foster.update(is_deleted)
   end
-  
-  
+
+
   private
   def foster_params
     params.require(:foster).permit(:last_name, :first_name, :telephone_number, :postal_code, :address, :email)
-  end 
-  
+  end
+
 end
