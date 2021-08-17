d0c07fb779fc0934cac9765a553eeb422f2788ba
mfrkw <xjdk3766@gmail.com>
2021-08-17 02:23:25 +0000

    [いいね非同期化]

--- app/controllers/member/likes_controller.rb
+++ app/controllers/member/likes_controller.rb
@@ -1,16 +1,16 @@
 class Member::LikesController < ApplicationController
-  before_action :authenticate_member!, except: :index
+  before_action :authenticate_member!
 
   def create
     @like = current_member.likes.create(dog_id: params[:dog_id])
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path) 非同期化
   end
 
   def destroy
     @dog = Dog.find(params[:dog_id])
     @like = current_member.likes.find_by(dog_id: @dog.id)
     @like.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
 end
